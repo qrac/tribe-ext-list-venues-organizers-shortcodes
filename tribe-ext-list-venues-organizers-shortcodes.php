fde2b1dfc852cabf604c2fa41b6c51986886ed1b
Clifford Paulick <tko@tourkick.com>
2018-03-19 18:49:16 -0500

    Reformat in PhpStorm

--- tribe-ext-list-venues-organizers-shortcodes.php
+++ tribe-ext-list-venues-organizers-shortcodes.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin name: 	The Events Calendar Extension: List Venues/Organizers Shortcodes
- * Description: 	Adds the `[list_venues]` and `[list_organizers]` shortcodes to list Venues and Organizers.
- * Version:     	2.0.0
+ * Plugin name:     The Events Calendar Extension: List Venues/Organizers Shortcodes
+ * Description:     Adds the `[list_venues]` and `[list_organizers]` shortcodes to list Venues and Organizers.
+ * Version:         2.0.0
  * Extension Class: Tribe__Extension__VenueOrganizer_List
- * Author:      	Modern Tribe, Inc
- * Author URI:  	http://theeventscalendar.com
- * License:     	GPL version 3 or any later version
+ * Author:          Modern Tribe, Inc
+ * Author URI:      http://theeventscalendar.com
+ * License:         GPL version 3 or any later version
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain: 	tribe-ext-list-venues-organizers-shortcodes
+ * Text Domain:     tribe-ext-list-venues-organizers-shortcodes
  *
-  *     This plugin is free software: you can redistribute it and/or modify
+ *     This plugin is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     any later version.
@@ -60,7 +60,7 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 			// remove all whitespaces
 			$exclude = preg_replace( '/\s+/', '', $this->atts['exclude'] );
 			$include = preg_replace( '/\s+/', '', $this->atts['include'] );
-			$author = preg_replace( '/\s+/', '', $this->atts['author'] );
+			$author  = preg_replace( '/\s+/', '', $this->atts['author'] );
 			if ( 'current_user' == $author ) {
 				$author = get_current_user_id();
 			}
@@ -70,13 +70,17 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 			$args = array(
 				'post_type'      => $this->atts['post_type'],
 				'posts_per_page' => $this->atts['limit'],
-				'order'			 => $this->atts['order'],
-				'orderby'		 => $this->atts['orderby'],
-				'post__not_in'	 => $exclude, // must be an array
+				'order'          => $this->atts['order'],
+				'orderby'        => $this->atts['orderby'],
+				'post__not_in'   => $exclude, // must be an array
 			);
 
-			if ( $this->atts['include'] ) { $args['post__in'] = $include; }
-			if ( $this->atts['author'] ) { $args['author'] = $author; }
+			if ( $this->atts['include'] ) {
+				$args['post__in'] = $include;
+			}
+			if ( $this->atts['author'] ) {
+				$args['author'] = $author;
+			}
 
 			$this->query = new WP_Query( apply_filters( 'tribe_ext_list_venues_organizers_shortcodes_args', $args, $this->atts ) );
 		}
@@ -86,15 +90,15 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 			$this->output = apply_filters( 'tribe_ext_list_venues_organizers_shortcodes_list_open', $opening_tag, $this->atts );
 
 			// remove all whitespaces
-			$thumb 		= preg_replace( '/\s+/', '', $this->atts['thumb'] );
-			$details 	= preg_replace( '/\s+/', '', $this->atts['details'] );
-			$count 		= preg_replace( '/\s+/', '', $this->atts['count'] );
+			$thumb      = preg_replace( '/\s+/', '', $this->atts['thumb'] );
+			$details    = preg_replace( '/\s+/', '', $this->atts['details'] );
+			$count      = preg_replace( '/\s+/', '', $this->atts['count'] );
 			$hide_empty = preg_replace( '/\s+/', '', $this->atts['hide_empty'] );
 
 			while ( $this->query->have_posts() ) {
 				$this->query->the_post();
 				$shortcode_type = 'tribe_organizer' == $this->atts['post_type'] ? 'organizer' : 'venue';
-				$id = get_the_ID();
+				$id             = get_the_ID();
 
 				// count upcoming events
 				$args = array(
@@ -107,19 +111,19 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 					$args['venue'] = $id;
 				}
 
-				$events = tribe_get_events ( $args );
+				$events       = tribe_get_events( $args );
 				$events_count = count( $events );
 
 				// generate HTML
 				if ( 'yes' != $hide_empty || ( 'yes' == $hide_empty && $events_count > 0 ) ) :
 
-					$item = '<li id="' . esc_attr( $this->atts['post_type'] . '-' . $id ) . '" class="tec list ' . $this->atts['post_type'] . '">';
+					$item       = '<li id="' . esc_attr( $this->atts['post_type'] . '-' . $id ) . '" class="tec list ' . $this->atts['post_type'] . '">';
 					$item_thumb = '';
 
 					// get the post thumbnail (if there is one)
 					if ( 'yes' == $thumb ) {
 						if ( get_the_post_thumbnail( $id ) ) {
-							$image = '<div class="tribe-venues-organizers-image">' . get_the_post_thumbnail( $id, 'thumbnail' ) . '</div>';
+							$image      = '<div class="tribe-venues-organizers-image">' . get_the_post_thumbnail( $id, 'thumbnail' ) . '</div>';
 							$item_thumb = $image;
 						}
 					}
@@ -142,7 +146,7 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 						}
 
 						$count_string = '<span class="tribe-venues-organizers-event-count">' . $count_string . '</span>';
-						apply_filters ( 'tribe_venue_organizer_shortcode_count', $events_count, $count_string );
+						apply_filters( 'tribe_venue_organizer_shortcode_count', $events_count, $count_string );
 						$items['count_string'] = $count_string;
 					}
 
@@ -156,8 +160,8 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 							apply_filters( 'tribe_organizer_shortcode_details', $details );
 						} else {
 							$venue_details = tribe_get_venue_details();
-							$item_details = $venue_details['address'];
-							$item_details = '<p class="tribe-venue-details">' . $item_details . '</p>';
+							$item_details  = $venue_details['address'];
+							$item_details  = '<p class="tribe-venue-details">' . $item_details . '</p>';
 							apply_filters( 'tribe_venue_shortcode_details', $details );
 						}
 
@@ -177,30 +181,31 @@ if ( class_exists( 'Tribe__Extension' ) && ! class_exists( 'Tribe__Extension__Ve
 		}
 
 		public function tec_do_venue_shortcode( $atts ) {
-			return 	$this->do_shortcode( Tribe__Events__Main::VENUE_POST_TYPE, $atts );
+			return $this->do_shortcode( Tribe__Events__Main::VENUE_POST_TYPE, $atts );
 		}
 
 		public function tec_do_organizer_shortcode( $atts ) {
-			return 	$this->do_shortcode( Tribe__Events__Main::ORGANIZER_POST_TYPE, $atts );
+			return $this->do_shortcode( Tribe__Events__Main::ORGANIZER_POST_TYPE, $atts );
 		}
 
 		private function do_shortcode( $post_type, $atts ) {
 			$this->atts = shortcode_atts( array(
-				'post_type' => $post_type,
-				'limit'     => -1,
-				'order'		=> 'ASC',
-				'orderby'	=> 'post_title',
-				'exclude'	=> '', // comma-separated list of Venue post IDs or Organizer post IDs
-				'include'	=> '', // comma-separated list of Venue post IDs or Organizer post IDs
-				'author' 	=> '', // int || str Venue or Organizer author ID or str "current_user"
-				'thumb'		=> '', // str "yes" to include Venue or Organizer thumbnail
-				'details'	=> '', // str "yes" to include Venue or Organizer details
-				'count' 	=> '', // str "yes" to include count of upcoming events for each Venue or Organizer
+				'post_type'  => $post_type,
+				'limit'      => - 1,
+				'order'      => 'ASC',
+				'orderby'    => 'post_title',
+				'exclude'    => '', // comma-separated list of Venue post IDs or Organizer post IDs
+				'include'    => '', // comma-separated list of Venue post IDs or Organizer post IDs
+				'author'     => '', // int || str Venue or Organizer author ID or str "current_user"
+				'thumb'      => '', // str "yes" to include Venue or Organizer thumbnail
+				'details'    => '', // str "yes" to include Venue or Organizer details
+				'count'      => '', // str "yes" to include count of upcoming events for each Venue or Organizer
 				'hide_empty' => '', // string "yes" to exclude Venues or Organizers without upcoming events
 			), $atts );
 
 			$this->query();
 			$this->format();
+
 			return $this->output;
 		}
 	}
